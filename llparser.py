2004372e00d3c8a75d22f653803770a4b6173df3
Uladzislau Khamkou <kham@tuta.io>
2022-02-22 21:51:35 +0300

    put parser logic into Parser class

--- llparser.py
+++ llparser.py
@@ -1,5 +1,6 @@
 from lookaheaditerators import LookAheadIterator
-from grammar import ε, τ
+from dataclasses import dataclass
+from grammar import Grammar, ε, τ
 
 
 def construct_table(grm):
@@ -18,21 +19,30 @@ def construct_table(grm):
     return table
 
 
-def parser(grm):
-    terminals = grm.terminals
-    table = construct_table(grm)
-    rules = grm.rules
-    prefixes = grm.prefixes()
+@dataclass
+class Parser:
+    grammar: Grammar
+    table: {(str, str): int}
 
-    def parse_symbol(source, symbol):
-        if symbol in terminals:
+    def __init__(self, grammar, table=None):
+        self.table = table or construct_table(grammar)
+        self.grammar = grammar
+
+    def parse_symbol(self, source, symbol):
+        rules, table = self.grammar.rules, self.table
+        if symbol in self.grammar.terminals:
             if symbol != source.next:
                 raise SyntaxError(f"Expected {symbol}")
             return next(source)
         state = (symbol, source.next)
         if state not in table:
             raise SyntaxError(f"Unexpected token: {source.next}")
-        fields = tuple(parse_symbol(source, s) for s in rules[table[state]][1])
+        rule = rules[table[state]][1]
+        fields = tuple(self.parse_symbol(source, symbol) for symbol in rule)
         return (symbol, fields)
 
-    return lambda src, sm=rules[0][0]: parse_symbol(LookAheadIterator(src), sm)
+    def parse(self, source, symbol=None):
+        symbol = symbol if symbol is not None else self.grammar.rules[0][0]
+        if not (isinstance(source, LookAheadIterator)):
+            source = LookAheadIterator(source)
+        return self.parse_symbol(source, symbol)
