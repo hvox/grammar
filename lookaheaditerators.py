7740fd5c8d0238f92841b37bee43ce7e2230ce83
Uladzislau Khamkou <kham@tuta.io>
2022-02-22 20:03:25 +0300

    support property "finished" for lookahead iterator

--- lookaheaditerators.py
+++ lookaheaditerators.py
@@ -1,28 +1,30 @@
 class LookAheadIterator:
     def __init__(self, it):
         self.iterator = iter(it)
+        self.finished = False
         self.next = None
         self.goto_next()
 
     def goto_next(self):
         current = self.next
         try:
-            self.next = (next(self.iterator),)
+            self.next = next(self.iterator)
         except StopIteration:
             self.next = None
+            self.finished = True
         return current
 
     def get_next(self):
         elem = self.next
-        return None if elem is None else elem[0]
+        return elem
 
     def __next__(self):
         elem = self.goto_next()
-        return None if elem is None else elem[0]
+        return elem
 
     def __iter__(self):
         while True:
-            current = self.goto_next()
-            if current is None:
+            if self.finished:
                 return
-            yield current[0]
+            current = self.goto_next()
+            yield current
