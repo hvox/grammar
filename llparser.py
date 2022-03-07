0c57ec8e29e08bf00299024df1a49cde3225eefe
Uladzislau Khamkou <kham@tuta.io>
2022-03-07 15:07:41 +0300

    don't include symbol names in arguments to actions

--- llparser.py
+++ llparser.py
@@ -42,7 +42,7 @@ class Parser:
             raise SyntaxError(f"Unexpected token: {source.next}")
         rule = rules[table[state]][1]
         fields = tuple(self.parse_symbol(source, symbol) for symbol in rule)
-        return self.actions.get(symbol, lambda *f: f)(symbol, *fields)
+        return self.actions.get(symbol, lambda *f: (symbol, f))(*fields)
 
     def parse(self, source, symbol=None):
         symbol = symbol if symbol is not None else self.grammar.rules[0][0]
