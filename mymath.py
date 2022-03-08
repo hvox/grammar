35d71bc45dd50bd457d790a5756f21c988b2fe80
Uladzislau Khamkou <kham@tuta.io>
2022-03-08 13:43:04 +0300

    implement nice rational numbers

--- mymath.py
+++ mymath.py
@@ -1,4 +1,12 @@
 from operator import add, sub, mul
+from fractions import Fraction
+
+
+class Rational(Fraction):
+    def __str__(self):
+        if self.denominator == 1:
+            return str(self.numerator)
+        return f"{self.numerator}/{self.denominator}"
 
 
 def sign(x):
