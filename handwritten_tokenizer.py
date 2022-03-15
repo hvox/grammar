a587e199894c0ca1e6d1d3d477d403b50d3902b8
Uladzislau Khamkou <kham@tuta.io>
2022-03-15 09:47:46 +0300

    implement join and split for handwritten tokenizer

--- handwritten_tokenizer.py
+++ handwritten_tokenizer.py
@@ -6,3 +6,25 @@ class Tokens(enum.Enum):
     keyword = 2
     identifier = 3
     newline = 4
+
+
+def join(it, delimiter=None):
+    it = iter(it)
+    try:
+        result = next(it)
+    except StopIteration:
+        return []
+    for el in it:
+        if delimiter:
+            result += delimiter
+        result += el
+    return result
+
+def split(s, delimiter):
+    j = 0
+    for i in range(len(s)):
+        if s[i] == delimiter:
+            yield s[j:i]
+            j = i + 1
+    if len(s) and j != len(s):
+        yield s[j:len(s)]
