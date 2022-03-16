6c536e2ffdf01169e1c90e05c8d5aeaa3745dd4a
Uladzislau Khamkou <kham@tuta.io>
2022-03-16 13:57:58 +0300

    prettify code of handwritten tokenizer

--- handwritten_tokenizer.py
+++ handwritten_tokenizer.py
@@ -2,7 +2,9 @@ import enum
 from functools import cache
 from itertools import islice
 from string import digits, ascii_letters, ascii_lowercase
-identifier_chars = set(digits + ascii_letters + '_$')
+
+identifier_chars = set(digits + ascii_letters + "_$")
+
 
 class Tokens(enum.Enum):
     comment = enum.auto()
@@ -24,6 +26,7 @@ def join(it, delimiter=None):
         result += el
     return result
 
+
 def split(s, delimiter):
     j = 0
     for i in range(len(s)):
@@ -31,7 +34,8 @@ def split(s, delimiter):
             yield s[j:i]
             j = i + 1
     if len(s) and j != len(s):
-        yield s[j:len(s)]
+        yield s[j : len(s)]
+
 
 def parse_token(src, keywords):
     if src in keywords:
@@ -41,6 +45,7 @@ def parse_token(src, keywords):
     if src[0] not in digits and all(c in identifier_chars for c in src):
         return (Tokens.identifier, src)
     return None
+
 
 @cache
 def parse_word(src, keywords):
@@ -53,29 +58,33 @@ def parse_word(src, keywords):
         if left is not None and right is not None:
             return [left] + right
     return None
+
+
 def parse(source, keywords={}):
     keywords = frozenset(keywords)
     parse = lambda source: list(parse_word(source, keywords))
     tokens = []
-    #print(source, '-->', list(split(source, '\n')), source.split('\n'))
-    for line in source.split('\n'):
-        #print('!:', repr(line))
+    # print(source, '-->', list(split(source, '\n')), source.split('\n'))
+    for line in source.split("\n"):
+        # print('!:', repr(line))
         code, comment = line, []
-        if '#' in line:
-            code, comment = line.split('#', 1)
-            comment = [(Tokens.comment, '#'+comment)]
-        code = list(map(parse, code.strip().split(' '))) if code else []
+        if "#" in line:
+            code, comment = line.split("#", 1)
+            comment = [(Tokens.comment, "#" + comment)]
+        code = list(map(parse, code.strip().split(" "))) if code else []
         if any(t is None for t in code):
             return None
         tokens.append(join(code) + comment)
-    #print('tokens:', tokens)
-    return join(tokens, [(Tokens.newline, '\n')])
+    # print('tokens:', tokens)
+    return join(tokens, [(Tokens.newline, "\n")])
+
 
 def ascii_lowercase_words():
     yield from ascii_lowercase
     for word in ascii_lowercase_words():
         for letter in ascii_lowercase:
             yield word + letter
+
 
 def minificate(tokens):
     ignored_tokens = {Tokens.comment, Tokens.newline}
@@ -93,10 +102,12 @@ def minificate(tokens):
             tokens[i] = (Tokens.identifier, identifiers[t[1]])
     return tokens
 
+
 def ff(tokens):
     if tokens is None:
-        return 'NONE'
-    return ':'.join(repr(t[1]) for t in tokens)
+        return "NONE"
+    return ":".join(repr(t[1]) for t in tokens)
+
 
 def tokens_to_str(tokens, keywords):
     result = []
@@ -104,12 +115,12 @@ def tokens_to_str(tokens, keywords):
         if len(result) == 0:
             result.append([t])
             continue
-        y0 = parse(t[1] + ''.join(map(lambda x: x[1], result[-1])), keywords)
+        y0 = parse(t[1] + "".join(map(lambda x: x[1], result[-1])), keywords)
         y1 = [t] + result[-1]
-        #print('res:', t, '-', *map(ff, result))
-        #print(ff(y0), ff(y1))
+        # print('res:', t, '-', *map(ff, result))
+        # print(ff(y0), ff(y1))
         if y0 == y1:
             result[-1] = y0
         else:
             result.append([t])
-    return ' '.join(''.join(t[1] for t in w) for w in reversed(result))
+    return " ".join("".join(t[1] for t in w) for w in reversed(result))
