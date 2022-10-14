6a20e6dfcb30d40193e32ad9a57bc5fa8417c432
Uladzislau Khamkou <kham@tuta.io>
2022-10-14 23:27:55 +0300

    add library code necessary for grammar.py

---
+++ lib/deterministic.py
@@ -0,0 +1,29 @@
+from typing import TypeVar, Iterable, Iterator
+
+T = TypeVar("T")
+
+
+class DeterministicSet(Iterable[T]):
+    def __init__(self, elements: Iterable[T] = ()):
+        self.elements = dict.fromkeys(elements)
+
+    def __iter__(self) -> Iterator[T]:
+        yield from self.elements
+
+    def __sub__(self, other: "DeterministicSet[T] | set[T]"):
+        return DeterministicSet(x for x in self.elements if x not in other)
+
+    def __or__(self, other: "DeterministicSet[T] | set[T]"):
+        return DeterministicSet(self.elements | dict.fromkeys(other))
+
+    def __and__(self, other: "DeterministicSet[T] | set[T]"):
+        return DeterministicSet(x for x in self.elements if x in other)
+
+    def __contains__(self, x):
+        return x in self.elements
+
+    def __repr__(self):
+        return "{"  + ", ".join(map(repr, self.elements)) + "}"
+
+    def add(self, x: T):
+        self.elements[x] = None
