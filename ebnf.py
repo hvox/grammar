9eb61926fc799f41f1f09d15833724e81bb0102d
Uladzislau Khamkou <kham@tuta.io>
2022-11-11 13:35:01 +0300

    Use parser generator for EBNF parsing

--- ebnf.py
+++ ebnf.py
@@ -1,5 +1,8 @@
 import string
+from parser import lr_parser
+from pathlib import Path
 from typing import Any, Iterable
+from lexer import construct_lexer
 
 # from lib.deterministic import DeterministicSet as set
 
@@ -18,7 +21,7 @@ from typing import Any, Iterable
 
 class EBNF:
     def __init__(self, rules: Iterable[tuple[str, Any]]):
-        self.rules = {head: body for head, body in rules}
+        self.rules = dict(rules)
 
     def __repr__(self):
         def node_repr(node):
@@ -31,106 +34,44 @@ class EBNF:
                     return "[" + node_repr(value) + "]"
                 case "rep", value:
                     return "{" + node_repr(value) + "}"
-                case str(literal):
-                    return repr(literal)
+                case terminal:
+                    return terminal
         result = []
         for head, body in self.rules.items():
             result.append(f"{head} = {node_repr(body)};")
         return "\n".join(result)
 
 
-def skip_whitespaces(source: str, i: int = 0):
-    while i < len(source) and source[i] in " \t\n":
-        i += 1
-    return i
+scan_ebnf_tokens = construct_lexer({
+    r"\s+": None,
+    r"[-|,(){}\[\]=;]": lambda _, s: (s, s),
+    r"\w+(\s+\w+)*": lambda _, s: ("identifier", s),
+    r'"[^"]*"': lambda _, s: ("terminal", s),
+    r"'[^']*'": lambda _, s: ("terminal", s),
+})
+parse_ebnf_tokens = lr_parser({
+    ("defs", ("def", "defs")): lambda x, y: [x] + y,
+    ("defs", ("def",)): lambda x: [x],
+    ("def", ("identifier", "=", "alt", ";")): lambda var, _, expr, __: (var, expr),
+    ("alt", ("alt", "|", "cat")): lambda x, _, y: x + (y,) if x[0] == "alt" else ("alt", x, y),
+    ("alt", ("cat",)): lambda x: x,
+    ("cat", ("cat", ",", "term")): lambda x, _, y: x + (y,) if x[0] == "cat" else ("cat", x, y),
+    ("cat", ("term",)): lambda x: x,
+    ("term", ("terminal",)): lambda x: x,
+    ("term", ("identifier",)): lambda x: x,
+    ("term", ("[", "alt", "]")): lambda _, x, __: ("opt", x),
+    ("term", ("{", "alt", "}")): lambda _, x, __: ("rep", x),
+    ("term", ("(", "alt", ")")): lambda _, x, __: x,
+})
 
 
-def parse_optional(source: str, i: int = 0):
-    inside, i = parse_ebnf_alternation(source, skip_whitespaces(source, i + 1))
-    return ("opt", inside), skip_whitespaces(source, i + 1)
-
-
-def parse_repetition(source: str, i: int = 0):
-    inside, i = parse_ebnf_alternation(source, skip_whitespaces(source, i + 1))
-    return ("rep", inside), skip_whitespaces(source, i + 1)
-
-
-def parse_grouping(source: str, i: int = 0):
-    inside, i = parse_ebnf_alternation(source, skip_whitespaces(source, i + 1))
-    return inside, skip_whitespaces(source, i + 1)
-
-
-def parse_terminal(source: str, i: int = 0):
-    marker = source[i]
-    assert marker in "'\""
-    j = i + 1
-    while source[j] != marker:
-        j += 1
-    return source[i + 1: j], skip_whitespaces(source, j + 1)
-
-
-def parse_variable(source: str, i: int = 0):
-    j = i
-    while source[j] in string.ascii_letters + string.whitespace:
-        j += 1
-    return source[i: j].strip(), j
-
-
-def parse_ebnf_concatenation(source: str, i: int = 0):
-    body = []
-    while i < len(source):
-        if source[i] == "[":
-            term, i = parse_optional(source, i)
-        elif source[i] == "{":
-            term, i = parse_repetition(source, i)
-        elif source[i] == "(":
-            term, i = parse_grouping(source, i)
-        elif source[i] in "'\"":
-            term, i = parse_terminal(source, i)
-        elif source[i] in string.ascii_letters:
-            term, i = parse_variable(source, i)
-        else:
-            break
-        body.append(term)
-        if source[i] != ",":
-            break
-        i = skip_whitespaces(source, i + 1)
-    result = tuple(["cat"] + body) if len(body) > 2 else body[0]
-    return result, skip_whitespaces(source, i)
-
-
-def parse_ebnf_alternation(source: str, i: int = 0):
-    options = []
-    while i < len(source):
-        option, i = parse_ebnf_concatenation(source, skip_whitespaces(source, i))
-        options.append(option)
-        if source[i] != "|":
-            break
-        i = skip_whitespaces(source, i + 1)
-    result = tuple(["alt"] + options) if len(options) > 2 else options[0]
-    return result, skip_whitespaces(source, i)
-
-
-def parse_ebnf_rule(source: str, i: int = 0):
-    head, i = parse_variable(source, i)
-    assert source[i] == "="
-    i = skip_whitespaces(source, i + 1)
-    body, i = parse_ebnf_alternation(source, i)
-    return (head, body), i
-
-
-def parse_ebnf(source: str) -> EBNF:
-    i = 0
+def parse_ebnf(source: str, i: int = 0):
     rules = {}
-    source = source.replace("\n", " ").strip()
-    while i < len(source):
-        (head, body), i = parse_ebnf_rule(source, skip_whitespaces(source, i))
-        rules[head] = body
-        if source[i] != ";":
-            break
-        i = skip_whitespaces(source, i + 1)
-    return EBNF(rules.items())
+    for variable, definition in parse_ebnf_tokens(scan_ebnf_tokens(source, i)):
+        if variable in rules:
+            raise ValueError(f"variable {variable} is defined twise")
+        rules[variable] = definition
+    return EBNF(rules)
 
 
-from pathlib import Path
 print(parse_ebnf(Path("./ebnf.ebnf").read_text()))
