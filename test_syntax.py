19420a0160919b508fbb80d589921429fe916059
Uladzislau Khamkou <kham@tuta.io>
2022-12-30 21:50:28 +0300

    Add more tests of Syntax class functionality

--- test_syntax.py
+++ test_syntax.py
@@ -46,3 +46,33 @@ def test_clr_table_generation():
     actions, gotos = Syntax(rules).get_clr_parsing_table(root_node="S")
     assert actions == expected_action_table
     assert gotos == expected_goto_table
+
+
+def test_rule_parsing():
+    rules = {
+        "S C C": None,
+        "C c C": None,
+        "C d": None,
+    }
+    expected_action_table = {
+        (0, "c"): ("shift", 3),
+        (0, "d"): ("shift", 4),
+        (1, None): ("accept",),
+        (2, "c"): ("shift", 6),
+        (2, "d"): ("shift", 7),
+        (3, "c"): ("shift", 3),
+        (3, "d"): ("shift", 4),
+        (4, "d"): ("reduce", Rule(head="C", body=("d",))),
+        (4, "c"): ("reduce", Rule(head="C", body=("d",))),
+        (5, None): ("reduce", Rule(head="S", body=("C", "C"))),
+        (6, "c"): ("shift", 6),
+        (6, "d"): ("shift", 7),
+        (7, None): ("reduce", Rule(head="C", body=("d",))),
+        (8, "d"): ("reduce", Rule(head="C", body=("c", "C"))),
+        (8, "c"): ("reduce", Rule(head="C", body=("c", "C"))),
+        (9, None): ("reduce", Rule(head="C", body=("c", "C"))),
+    }
+    expected_goto_table = {(0, "S"): 1, (0, "C"): 2, (2, "C"): 5, (3, "C"): 8, (6, "C"): 9}
+    actions, gotos = Syntax(rules).get_clr_parsing_table(root_node="S")
+    assert actions == expected_action_table
+    assert gotos == expected_goto_table
